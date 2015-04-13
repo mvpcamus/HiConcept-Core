672461516a4b19bc6f34be489f1cb52a18d4a292
Jun Jo <camus@etri.re.kr>
2015-04-13 15:51:51 +0900

    revise error handler design - fix uncaught exception of null log directory @ logger.js - revise uncaught exception handler @ index.js - add process termination message @ index.js - update error codes & messages @ errCodes.json

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-global.__homePath = __dirname;
+#!/usr/bin/env node
+
+global._homePath = __dirname;
 // modules
-var server = require(__homePath+'/lib/server.js');
-var router = require(__homePath+'/lib/router.js');
-var errHandler = require(__homePath+'/lib/errHandler.js');
-var readConfig = require(__homePath+'/lib/readConfig.js');
-var initialize = require(__homePath+'/lib/initialize.js');
-var requestHandler = require(__homePath+'/lib/requestHandler.js');
+var server = require(_homePath+'/lib/server.js');
+var router = require(_homePath+'/lib/router.js');
+var errHandler = require(_homePath+'/lib/errHandler.js');
+var readConfig = require(_homePath+'/lib/readConfig.js');
+var initialize = require(_homePath+'/lib/initialize.js');
+var requestHandler = require(_homePath+'/lib/requestHandler.js');
 
 // define router handles
 var handle = {};
@@ -15,40 +17,56 @@ handle['/publish'] = requestHandler.publish;
 handle['/subscribe'] = requestHandler.subscribe;
 
 process.on('uncaughtException', function(error) {
-    errHandler('FX::'+error);
-    if (typeof error === 'object' && error.code == 'EACCES') {
-        errHandler('H02::'+config.port);
-        errHandler('H03');
-    }
-    if (typeof error === 'object' && error.code == 'ENOENT') {
-        var fs = require('fs');
-        if (!fs.existsSync(__homePath+'/log/')) {
-            errHandler('EE51');
-            fs.mkdirSync(__homePath+'/log',0775);
-            errHandler('IE52');
-            errHandler('H04');
+    if (typeof error === 'object') {
+        errHandler('FX::'+error.stack);
+        if (error.code == 'EACCES') {
+            errHandler('H02::['+_config.port+']');
+            errHandler('H03');
         }
+    } else {
+        error += '';
+        errHandler('FX::'+error);
     }
 });
 
+process.on('exit', function(code) {
+    if (code) {
+        errHandler('F0::Terminate Process: '+process.title+'('+code+')'
+             +', PID: '+process.pid+', Uptime: '+process.uptime()+'s');
+    } else {
+        errHandler('I0::Terminate Process: '+process.title+'('+code+')'
+             +', PID: '+process.pid+', Uptime: '+process.uptime()+'s');
+    }
+});
+
+process.on('SIGINT', function() {
+    console.log('');
+    process.exit(0);
+});
+
 // load config >> initialization >> server start
-readConfig(__homePath+'/config/hiconcpt.yml', function(error, config) {
+readConfig(function(error, config) {
     if(error) {
         errHandler(error);
     } else {
         errHandler('I100'); // configuration check [OK]
-        global.config = config;
+        global._config = config;
         // start initialization
         initialize(function(error, admin) {
             if(error) {
                 errHandler(error);
             } else {
                 errHandler('I14'); // completed successfully
-errHandler('T0::'+JSON.stringify(config)); //TODO remove
+//errHandler('T0::'+JSON.stringify(_config)+'\n'+JSON.stringify(process.env)); //TODO remove
                 //start web server
                 setTimeout(function() {
                     server.start(router.route, handle, function(error) {
-                        errHandler(error);
+                        if(error) {
+                            errHandler(error);
+                        } else {
+                            errHandler('I200::['+_config.port+']'); // server started
+                            errHandler('H04');
+                        }
                     });
                 },1000);
             }
